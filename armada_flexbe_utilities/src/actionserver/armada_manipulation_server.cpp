073dbe07df0ed5f9dd570e7a4f9d7b430bfd96e8
brian <flynn.m.brian@gmail.com>
2022-02-24 14:06:46 -0500

    edit: outline action functionality with old function, begin modification

--- armada_flexbe_utilities/src/actionserver/armada_manipulation_server.cpp
+++ armada_flexbe_utilities/src/actionserver/armada_manipulation_server.cpp
@@ -52,7 +52,43 @@ public:
 
   void executeCartesianPlan(const armada_flexbe_utilities::CartesianMoveGoalConstPtr &goal)
   {
-    // do things
+    std::vector<geometry_msgs::Pose> pre_and_grasp_pose_list;
+    std::vector<geometry_msgs::Pose> retreat_pose_list;
+    moveit::planning_interface::MoveGroupInterface::Plan pre_and_grasp_pose_plan;
+    moveit::planning_interface::MoveGroupInterface::Plan retreat_pose_plan;
+
+    armada_flexbe_utilities::GraspPosesList poses_list;
+    poses_list = goal->pose_list;
+
+    bool temp_flag;
+
+    unsigned long n = sizeof(poses_list);
+    for (unsigned long i = 0; i < n; ++i) {
+      pre_and_grasp_pose_list.clear();
+      //pre_and_grasp_pose_list.push_back(poses_list.poses[i].pre);
+      //pre_and_grasp_pose_list.push_back(poses_list.poses[i].target);
+      retreat_pose_list.clear();
+      //retreat_pose_list.push_back(poses_list[i].post);
+      // get cartesian plan for pre and actual grasp, if good execute and close gripper
+      double pre_and_grasp_success = cartesianPlan(pre_and_grasp_pose_list, pre_and_grasp_pose_plan);
+      if (pre_and_grasp_success == 100) {
+        MoveGroupPtr_->execute(pre_and_grasp_pose_plan);
+        ros::Duration(0.5).sleep();
+        //setGripper(0.5);
+        temp_flag = 1;
+      }
+      ros::Duration(.5).sleep();
+      // get cartesian plan for post grasp, if good execute
+      if (temp_flag) {
+        double post_success = cartesianPlan(retreat_pose_list, retreat_pose_plan);
+        if (post_success == 100) {
+          //move_group_ptr->execute(retreat_pose_plan);
+          ros::Duration(0.5).sleep();
+          break;
+        }
+      }
+      ros::Duration(0.5).sleep();
+    }
   }
 
 };
