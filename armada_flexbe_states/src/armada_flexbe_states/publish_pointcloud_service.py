03c8dd9da454a75b0233155722b7ee00ca22eaac
brian <flynn.m.brian@gmail.com>
2022-05-09 13:52:34 -0400

    add: flexbe pointcloud publisher state | remove: old snapshot action state | edit: delete some unnecessary lines in concatenate pointcloud state

--- armada_flexbe_states/src/armada_flexbe_states/publish_pointcloud_service.py
+++ armada_flexbe_states/src/armada_flexbe_states/publish_pointcloud_service.py
@@ -2,50 +2,50 @@
 import rospy
 
 from flexbe_core import EventState, Logger
-from flexbe_core.proxy import ProxyServiceCaller
+from flexbe_core.proxy import ProxyPublisher
 from sensor_msgs.msg import PointCloud2
 
 from armada_flexbe_utilities.srv import ConcatenatePointCloud, ConcatenatePointCloudResponse, ConcatenatePointCloudRequest
 
 
-class concatenatePointCloudState(EventState):
+class publishPointCloudState(EventState):
         '''
         Example for a state to demonstrate which functionality is available for state implementation.
         This example lets the behavior wait until the given target_time has passed since the behavior has been started.
 
-        ># pointcloud_list                              List of PointCloud2 messages
-        #> combined_pointcloud                          Concatenated PointCloud2 message
+        -- topic                                        Topic to publish pointcloud on
+
+        ># pointcloud                                   List of PointCloud2 messages
 
         <= continue                                     concatenated pointclouds successfully
         <= failed                                       something went wrong
 
         '''
 
-        def __init__(self):
+        def __init__(self, topic):
                 # Declare outcomes, input_keys, and output_keys by calling the super constructor with the corresponding arguments.
-                super(concatenatePointCloudState, self).__init__(outcomes = ['continue', 'failed'],
-                                                       input_keys = ['pointcloud_list'],
-                                                       output_keys = ['combined_pointcloud'])
+                super(publishPointCloudState, self).__init__(outcomes = ['continue', 'failed'],
+                                                       input_keys = ['pointcloud'])
+
+                self._topic = topic
+                self._pub = ProxyPublisher({self._topic: PointCloud2})
 
         def execute(self, userdata):
                 # This method is called periodically while the state is active.
                 # Main purpose is to check state conditions and trigger a corresponding outcome.
                 # If no outcome is returned, the state will stay active.
 
-                rospy.wait_for_service('/concatenate_pointcloud')
-                self._service_topic = '/concatenate_pointcloud'
-                self._service = ProxyServiceCaller({self._service_topic: ConcatenatePointCloud})
-                service_response = self._service.call(self._service_topic, userdata.pointcloud_list)
-                userdata.combined_pointcloud = service_response.cloud_out
-
                 return 'continue'
-
 
         def on_enter(self, userdata):
                 # This method is called when the state becomes active, i.e. a transition from another state to this one is taken.
                 # It is primarily used to start actions which are associated with this state.
 
-                Logger.loginfo('attempting to concatenate pointcloud...' )
+                Logger.loginfo('attempting to publish concatenated pointcloud...' )
+
+                val = PointCloud2()
+                val = userdata.pointcloud
+                self._pub.publish(self._topic, val)
 
         def on_exit(self, userdata):
                 # This method is called when an outcome is returned and another state gets active.
