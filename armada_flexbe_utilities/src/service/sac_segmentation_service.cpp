da0325864df5845efc9a2bd1f7844725388233d2
brian <flynn.m.brian@gmail.com>
2022-05-09 17:18:54 -0400

    edit: troubleshooting passthrough filter service, minor modifcations to sac segmentation service, slight modification to launch file, uncomment srv files in CMakeLists to rebuild

--- armada_flexbe_utilities/src/service/sac_segmentation_service.cpp
+++ armada_flexbe_utilities/src/service/sac_segmentation_service.cpp
@@ -12,116 +12,52 @@
 
 using namespace pcl;
 
-class SacSegmentationService
+/**
+ * Perform SAC (planar) segmentation on a PointCloud2 message.
+ *
+ * Given a PointCloud2 message, perform SAC (planar) segmentation and provide the resulting PointCloud2 message.
+ * More information about pcl filters at: https://pcl.readthedocs.io/projects/tutorials/en/master/#
+ *
+ * @param[in] req sensor_msgs/PointCloud2 A PointCloud2 message.
+ * @param[out] res sensor_msgs/PointCloud2 A PointCloud2 message.
+ * @return Bool Service completion result.
+ */
+bool sacSegmentation(armada_flexbe_utilities::SacSegmentation::Request &req,
+                     armada_flexbe_utilities::SacSegmentation::Response &res)
 {
-protected:
+  ROS_WARN("Executing SacSegmentation Service");
+  PointCloud<PointXYZRGB>::Ptr temp_cloud(new PointCloud<PointXYZRGB>);
+  fromROSMsg(req.cloud_in, *temp_cloud);
 
-  ros::ServiceServer sacSegmentationService;
+  ModelCoefficients::Ptr coefficients_plane (new ModelCoefficients);
+  PointIndices::Ptr inliers_plane (new PointIndices);
+  SACSegmentation<PointXYZRGB> seg;
+  seg.setOptimizeCoefficients (true);
+  seg.setModelType (SACMODEL_PLANE);
+  seg.setMethodType (SAC_RANSAC);
+  seg.setMaxIterations (1000);
+  seg.setDistanceThreshold (0.01);
+  seg.setInputCloud (temp_cloud);
+  seg.segment (*inliers_plane, *coefficients_plane);
 
-public:
+  ExtractIndices<PointXYZRGB> extract_indices;
+  extract_indices.setInputCloud(temp_cloud);
+  extract_indices.setIndices(inliers_plane);
+  extract_indices.setNegative(true);
+  extract_indices.filter(*temp_cloud);
 
-  /**
-   * Class Constructor.
-   *
-   * Constructor for SacSegmentationService class.
-   *
-   * @param[in] nh A ROS NodeHandle object.
-   */
-  SacSegmentationService(ros::NodeHandle nh)
-  {
-    sacSegmentationService = nh.advertiseService("sac_segmentation", &SacSegmentationService::sacSegmentation, this);
-  }
-
-  /**
-   * Class Destructor.
-   *
-   * Destructor for SacSegmentationService class.
-   */
-  ~SacSegmentationService(void)
-  {
-  }
-
-  /**
-   * Perform SAC (planar) segmentation on a PointCloud2 message.
-   *
-   * Given a PointCloud2 message, perform SAC (planar) segmentation and provide the resulting PointCloud2 message.
-   * More information about pcl filters at: https://pcl.readthedocs.io/projects/tutorials/en/master/#
-   *
-   * @param[in] req sensor_msgs/PointCloud2 A PointCloud2 message.
-   * @param[out] res sensor_msgs/PointCloud2 A PointCloud2 message.
-   * @return Bool Service completion result.
-   */
-  bool sacSegmentation(armada_flexbe_utilities::SacSegmentation::Request &req,
-                       armada_flexbe_utilities::SacSegmentation::Response &res)
-  {
-    ROS_WARN("Executing SacSegmentation Service");
-    PointCloud<PointXYZRGB>::Ptr temp_cloud(new PointCloud<PointXYZRGB>);
-    *temp_cloud = transformCloud(req.cloud_in, "world");
-
-    ModelCoefficients::Ptr coefficients_plane (new ModelCoefficients);
-    PointIndices::Ptr inliers_plane (new PointIndices);
-    SACSegmentation<PointXYZRGB> seg;
-    seg.setOptimizeCoefficients (true);
-    seg.setModelType (SACMODEL_PLANE);
-    seg.setMethodType (SAC_RANSAC);
-    seg.setMaxIterations (1000);
-    seg.setDistanceThreshold (0.01);
-    seg.setInputCloud (temp_cloud);
-    seg.segment (*inliers_plane, *coefficients_plane);
-
-    ExtractIndices<PointXYZRGB> extract_indices;
-    extract_indices.setInputCloud(temp_cloud);
-    extract_indices.setIndices(inliers_plane);
-    extract_indices.setNegative(true);
-    extract_indices.filter(*temp_cloud);
-
-    toROSMsg(*temp_cloud, res.cloud_out);
-    ROS_WARN("Finishing SacSegmentation Service");
-    return true;
-  }
-
-  /**
-   * Transform a PointCloud2 message into a PointCloud object.
-   *
-   * Given a PointCloud2 message and a target frame, transform it into a PointCloud<T> object in the target frame.
-   *
-   * @param[in] cloud sensor_msgs::PointCloud2 A PointCloud2 message.
-   * @param[in] target_frame std::string The target object transformation frame.
-   * @return PointCloud<PointXYZRGB> A PointCloud object.
-   */
-    PointCloud<PointXYZRGB> transformCloud(sensor_msgs::PointCloud2 cloud, std::string target_frame){
-    PointCloud<PointXYZRGB> temp_cloud;
-    fromROSMsg(cloud, temp_cloud);
-    boost::shared_ptr<tf::TransformListener> listener;
-
-    ros::Time stamp = ros::Time(0);
-    tf::StampedTransform transform;
-
-    pcl_conversions::toPCL(stamp, temp_cloud.header.stamp);
-
-    try
-    {
-      listener->waitForTransform(target_frame, temp_cloud.header.frame_id, stamp, ros::Duration(10.0));
-      listener->lookupTransform(target_frame, temp_cloud.header.frame_id, stamp, transform);
-    } catch (tf::TransformException err)
-    {
-      ROS_ERROR("%s", err.what());
-    }
-
-    pcl_ros::transformPointCloud(target_frame, temp_cloud, temp_cloud, *listener);
-
-    return temp_cloud;
-  }
-
-};
-
+  toROSMsg(*temp_cloud, res.cloud_out);
+  ROS_WARN("Finishing SacSegmentation Service");
+  return true;
+}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "sac_segmentation_service");
   ros::NodeHandle nh;
 
-  SacSegmentationService sacSegmentationService = SacSegmentationService(nh);
+  ros::ServiceServer sacSegmentationService = nh.advertiseService("sac_segmentation", sacSegmentation);
+  ROS_WARN("sac_segmentation_service Ready.");
   ros::spin();
 
   return 0;
