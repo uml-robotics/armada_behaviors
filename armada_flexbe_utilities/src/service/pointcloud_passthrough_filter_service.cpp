37982545bbba13e14e26177695594a1c4953a5fa
brian <flynn.m.brian@gmail.com>
2022-05-11 10:25:44 -0400

    edit: add informative warning messages for filtering (points before, after), add commented line for filter negatives for testing, will likely remain for awareness

--- armada_flexbe_utilities/src/service/pointcloud_passthrough_filter_service.cpp
+++ armada_flexbe_utilities/src/service/pointcloud_passthrough_filter_service.cpp
@@ -26,6 +26,7 @@ bool passthroughFilter(armada_flexbe_utilities::PointCloudPassthroughFilter::Req
                        armada_flexbe_utilities::PointCloudPassthroughFilter::Response &res)
 {
   ROS_WARN("Executing PassthroughFilter Service");
+  ROS_WARN_STREAM("Number of points in cloud before filter: " << req.cloud_in.data.size());
   PointCloud<PointXYZRGB>::Ptr temp_cloud(new PointCloud<PointXYZRGB>);
   fromROSMsg(req.cloud_in, *temp_cloud);
 
@@ -33,21 +34,25 @@ bool passthroughFilter(armada_flexbe_utilities::PointCloudPassthroughFilter::Req
   pass_x.setInputCloud (temp_cloud);
   pass_x.setFilterFieldName ("x");
   pass_x.setFilterLimits (req.x_min, req.x_max);
+  //pass_x.setFilterLimitsNegative(false);
   pass_x.filter(*temp_cloud);
 
   PassThrough<PointXYZRGB> pass_y;
   pass_y.setInputCloud (temp_cloud);
   pass_y.setFilterFieldName ("y");
   pass_y.setFilterLimits (req.y_min, req.y_max);
+  //pass_y.setFilterLimitsNegative(false);
   pass_y.filter(*temp_cloud);
 
   PassThrough<PointXYZRGB> pass_z;
   pass_z.setInputCloud (temp_cloud);
   pass_z.setFilterFieldName ("z");
   pass_z.setFilterLimits (req.z_min, req.z_max);
+  //pass_z.setFilterLimitsNegative(false);
   pass_z.filter(*temp_cloud);
 
   toROSMsg(*temp_cloud, res.cloud_out);
+  ROS_WARN_STREAM("Number of points in cloud after filter: " << res.cloud_out.data.size());
   ROS_WARN("Finishing PassthroughFilter Service");
   return true;
 }
