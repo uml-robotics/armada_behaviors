cb119678b70b4e0599014e13b731d3f072031697
Brian Flynn <flynn.m.brian@gmail.com>
2022-09-16 14:24:29 -0400

    add/edit: new filter functions, currently skeleton services

--- armada_flexbe_utilities/src/service/pcl_radius_outlier_removal_service.cpp
+++ armada_flexbe_utilities/src/service/pcl_radius_outlier_removal_service.cpp
@@ -1,23 +1,23 @@
 #include <ros/ros.h>
-#include "armada_flexbe_utilities/ConditionalOutlierRemoval.h"
+#include "armada_flexbe_utilities/RadiusOutlierRemoval.h"
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl/filters/conditional_removal.h>
+#include <pcl/filters/radius_outlier_removal.h>
 
 using namespace pcl;
 
 /**
- * Filter a PointCloud2 message by performing a conditional outlier removal process.
+ * Filter a PointCloud2 message by performing a radius outlier removal process.
  *
- * Given a PointCloud2 message, perform a conditional outlier removal process and provide the resulting PointCloud2 message.
+ * Given a PointCloud2 message, perform a radius outlier removal process and provide the resulting PointCloud2 message.
  * More information about pcl filters at: https://pcl.readthedocs.io/projects/tutorials/en/master/#
  *
  * @param[in] req sensor_msgs/PointCloud2 A PointCloud2 message.
  * @param[out] res sensor_msgs/PointCloud2 A PointCloud2 message.
  * @return Bool Service completion result.
  */
-bool conditionalOutlierRemoval(armada_flexbe_utilities::ConditionalOutlierRemoval::Request &req,
-                               armada_flexbe_utilities::ConditionalOutlierRemoval::Response &res)
+bool radiusOutlierRemoval(armada_flexbe_utilities::RadiusOutlierRemoval::Request &req,
+                          armada_flexbe_utilities::RadiusOutlierRemoval::Response &res)
 {
   //ROS_WARN_STREAM("Number of points in cloud before filter: " << req.cloud_in.data.size());
   PointCloud<PointXYZRGB>::Ptr temp_cloud(new PointCloud<PointXYZRGB>);
@@ -32,10 +32,10 @@ bool conditionalOutlierRemoval(armada_flexbe_utilities::ConditionalOutlierRemova
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "conditional_outlier_removal_service");
+  ros::init(argc, argv, "radius_outlier_removal_service");
   ros::NodeHandle nh;
 
-  ros::ServiceServer voxelGridFilterService = nh.advertiseService("conditional_outlier_removal", conditionalOutlierRemoval);
+  ros::ServiceServer voxelGridFilterService = nh.advertiseService("radius_outlier_removal", radiusOutlierRemoval);
   ros::spin();
 
   return 0;
---
+++ armada_flexbe_utilities/src/service/pcl_voxel_grid_filter_service.cpp
@@ -0,0 +1,42 @@
+#include <ros/ros.h>
+#include "armada_flexbe_utilities/VoxelGridFilter.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/voxel_grid.h>
+
+using namespace pcl;
+
+/**
+ * Downsample a PointCloud2 message by applying a voxelgrid filter.
+ *
+ * Given a PointCloud2 message, apply a voxelgrid filter and provide the resulting PointCloud2 message.
+ * More information about pcl filters at: https://pcl.readthedocs.io/projects/tutorials/en/master/#
+ *
+ * @param[in] req sensor_msgs/PointCloud2 A PointCloud2 message.
+ * @param[out] res sensor_msgs/PointCloud2 A PointCloud2 message.
+ * @return Bool Service completion result.
+ */
+bool voxelGridFilter(armada_flexbe_utilities::VoxelGridFilter::Request &req,
+                     armada_flexbe_utilities::VoxelGridFilter::Response &res)
+{
+  //ROS_WARN_STREAM("Number of points in cloud before filter: " << req.cloud_in.data.size());
+  PointCloud<PointXYZRGB>::Ptr temp_cloud(new PointCloud<PointXYZRGB>);
+  fromROSMsg(req.cloud_in, *temp_cloud);
+
+  // perform task here
+
+  toROSMsg(*temp_cloud, res.cloud_out);
+  //ROS_WARN_STREAM("Number of points in cloud after filter: " << res.cloud_out.data.size());
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "voxel_grid_filter_service");
+  ros::NodeHandle nh;
+
+  ros::ServiceServer voxelGridFilterService = nh.advertiseService("voxelgrid_filter", voxelGridFilter);
+  ros::spin();
+
+  return 0;
+}
