a4236dded88946b4429f00be02f9754b7558ae64
DanChoy <danchoy@gmail.com>
2022-10-05 12:40:21 -0400

    created a euclidean_cluster service that extract the largest item from pointcloud

--- armada_flexbe_utilities/src/service/euclidean_cluster_extraction_service.cpp
+++ armada_flexbe_utilities/src/service/euclidean_cluster_extraction_service.cpp
@@ -4,7 +4,7 @@
 #include <tf/transform_listener.h>
 //#include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
-//#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/passthrough.h>
 
@@ -57,98 +57,56 @@ public:
                                   armada_flexbe_utilities::EuclideanClusterExtraction::Response &res)
   {
     ROS_WARN("Executing EuclideanClusterExtraction Service");
-    pcl::PointCloud<PointXYZRGB>::Ptr temp_cloud(new PointCloud<PointXYZRGB>);
+    PointCloud<PointXYZRGB>::Ptr temp_cloud(new PointCloud<PointXYZRGB>);
+    PointCloud<PointXYZRGB>::Ptr desired_cluster (new PointCloud<PointXYZRGB>);
     fromROSMsg(req.cloud_in, *temp_cloud);
 
-    // Read in the cloud data
-//    pcl::PCDReader reader;
-//    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
-//    reader.read ("/home/csrobot/Downloads/table_scene_lms400.pcd", *cloud); //give this a real file path
-//    std::cout << "PointCloud before filtering has: " << cloud->size () << " data points." << std::endl; //*
 
     // Create the filtering object: downsample the dataset using a leaf size of 1cm
-//     pcl::VoxelGrid<pcl::PointXYZRGB> vg;
-//     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-//     vg.setInputCloud (temp_cloud);
-//     vg.setLeafSize (0.01f, 0.01f, 0.01f);
-//     vg.filter (*cloud_filtered);
-//     std::cout << "PointCloud after filtering has: " << cloud_filtered->size ()  << " data points." << std::endl; //*
+     VoxelGrid<PointXYZRGB> vg;
+     PointCloud<PointXYZRGB>::Ptr cloud_filtered (new PointCloud<PointXYZRGB>);
+     vg.setInputCloud (temp_cloud);
+     vg.setLeafSize (0.01f, 0.01f, 0.01f);
+     vg.filter (*cloud_filtered);
 
-//     // Create the segmentation object for the planar model and set all the parameters
-//     pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-//     pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-//     pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-//     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGB> ());
-//     pcl::PCDWriter writer;
-//     seg.setOptimizeCoefficients (true);
-//     seg.setModelType (pcl::SACMODEL_PLANE);
-//     seg.setMethodType (pcl::SAC_RANSAC);
-//     seg.setMaxIterations (100);
-//     seg.setDistanceThreshold (0.02);
-
-     int i=0, nr_points = (int) temp_cloud->size ();
-     while (temp_cloud->size () > 0.3 * nr_points)
-     {
-//       // Segment the largest planar component from the remaining cloud
-//       seg.setInputCloud (cloud_filtered);
-//       seg.segment (*inliers, *coefficients);
-//       if (inliers->indices.size () == 0)
-//       {
-//         std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
-//         break;
-//       }
-
-       // Extract the planar inliers from the input cloud
-       pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-       extract.setInputCloud (temp_cloud);
-//       extract.setIndices (inliers);
-       extract.setNegative (false);
-
-       // Get the points associated with the planar surface
-       extract.filter (*temp_cloud);
-       std::cout << "PointCloud representing the planar component: " << temp_cloud->size () << " data points." << std::endl;
-
-       // Remove the planar inliers, extract the rest
-//       extract.setNegative (true);
-//       extract.filter (*cloud_filtered);
-//       *cloud_filtered = *cloud_filtered;
-     }
 
      // Creating the KdTree object for the search method of the extraction
-     pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
-     tree->setInputCloud (temp_cloud);
+     search::KdTree<PointXYZRGB>::Ptr tree (new search::KdTree<PointXYZRGB>);
+     tree->setInputCloud (cloud_filtered);
 
-     std::vector<pcl::PointIndices> cluster_indices;
-     pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
+     std::vector<PointIndices> cluster_indices;
+     EuclideanClusterExtraction<PointXYZRGB> ec;
      ec.setClusterTolerance (0.02); // 2cm
      ec.setMinClusterSize (100);
      ec.setMaxClusterSize (25000);
      ec.setSearchMethod (tree);
-     ec.setInputCloud (temp_cloud);
+     ec.setInputCloud (cloud_filtered);
      ec.extract (cluster_indices);
 
-     int j = 0;
-     for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+     unsigned long j = 0;
+     int biggest_size = 0;
+     for (std::vector<PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
      {
-       pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
+       PointCloud<PointXYZRGB>::Ptr cloud_cluster (new PointCloud<PointXYZRGB>);
        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
-         cloud_cluster->push_back ((*temp_cloud)[*pit]); //*
+
+       cloud_cluster->push_back ((*cloud_filtered)[*pit]); //*
        cloud_cluster->width = cloud_cluster->size ();
        cloud_cluster->height = 1;
        cloud_cluster->is_dense = true;
+       cloud_cluster->header.frame_id = cloud_filtered->header.frame_id;
 
-//       std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size () << " data points." << std::endl;
-//       std::stringstream ss;
-//       ss << "cloud_cluster_" << j << ".pcd";
-//       writer.write<pcl::PointXYZRGB> (ss.str (), *cloud_cluster, false); //*
-//       j++;
-//        toROSMsg(cloud_cluster, res.cloud_out);
-       // string topic_name = ss;
-       // ros::Publisher<sensor_msgs::PointCloud2>(topic_name, 10); <- wrong
-       toROSMsg(*temp_cloud, res.cloud_list_out[j]);
+       int cluster_size = cloud_cluster->size();
+
+       if (biggest_size < cluster_size)
+       {
+        biggest_size = cluster_size;
+        desired_cluster = cloud_cluster;
+       }
      }
 
-    //toROSMsg(*temp_cloud, res.cloud_out);
+     toROSMsg(*desired_cluster, res.cluster_cloud);
+
     ROS_WARN("Finished EuclideanClusterExtraction Service");
     return true;
   }
