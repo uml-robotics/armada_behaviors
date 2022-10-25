7c5e6f2e7c0c70bae0717e4ebe735bc3dbcd7520
Brian Flynn <flynn.m.brian@gmail.com>
2022-10-25 10:47:12 -0400

    clean up #includes, remove unnecessary

--- armada_flexbe_utilities/src/service/euclidean_cluster_extraction_service.cpp
+++ armada_flexbe_utilities/src/service/euclidean_cluster_extraction_service.cpp
@@ -1,23 +1,8 @@
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl_ros/transforms.h>
-#include <tf/transform_listener.h>
-//#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/passthrough.h>
-
-#include <pcl/ModelCoefficients.h>
-#include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-
 #include "ros/ros.h"
 #include "armada_flexbe_utilities/EuclideanClusterExtraction.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 using namespace pcl;
 
