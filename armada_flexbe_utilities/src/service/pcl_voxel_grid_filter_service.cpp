9e65f49ff570f8265816893dca06415eb74c44b6
Brian Flynn <flynn.m.brian@gmail.com>
2022-10-25 10:41:04 -0400

    implement oop style, parameterize voxel grid leaf size variables

--- armada_flexbe_utilities/src/service/pcl_voxel_grid_filter_service.cpp
+++ armada_flexbe_utilities/src/service/pcl_voxel_grid_filter_service.cpp
@@ -6,39 +6,69 @@
 
 using namespace pcl;
 
-/**
- * Downsample a PointCloud2 message by applying a voxelgrid filter.
- *
- * Given a PointCloud2 message, apply a voxelgrid filter and provide the resulting PointCloud2 message.
- * More information about pcl filters at: https://pcl.readthedocs.io/projects/tutorials/en/master/#
- * This filter: https://pcl.readthedocs.io/projects/tutorials/en/latest/voxel_grid.html#voxelgrid
- *
- * @param[in] req sensor_msgs/PointCloud2 A PointCloud2 message.
- * @param[out] res sensor_msgs/PointCloud2 A PointCloud2 message.
- * @return Bool Service completion result.
- */
-bool voxelGridFilter(armada_flexbe_utilities::VoxelGridFilter::Request &req,
-                     armada_flexbe_utilities::VoxelGridFilter::Response &res)
+class VoxelGridFilterService
 {
-  PointCloud<PointXYZRGB>::Ptr input_cloud(new PointCloud<PointXYZRGB>);
-  PointCloud<PointXYZRGB>::Ptr filtered_cloud(new PointCloud<PointXYZRGB>);
-  fromROSMsg(req.cloud_in, *input_cloud);
+protected:
 
-  VoxelGrid<PointXYZRGB> vox;
-  vox.setInputCloud (input_cloud);
-  vox.setLeafSize (0.01f, 0.01f, 0.01f);
-  vox.filter (*filtered_cloud);
+  ros::NodeHandle nh_;
+  ros::ServiceServer voxelGridFilterService;
+  float leaf_size_x;
+  float leaf_size_y;
+  float leaf_size_z;
 
-  toROSMsg(*filtered_cloud, res.cloud_out);
-  return true;
-}
+public:
+
+  /**
+   * Class Constructor.
+   *
+   * Constructor for VoxelGridFilterService class.
+   *
+   * @param[in] nh A ROS NodeHandle object.
+   */
+  VoxelGridFilterService(ros::NodeHandle nh) :
+    nh_(nh)
+  {
+    voxelGridFilterService = nh.advertiseService("voxelgrid_filter", &VoxelGridFilterService::voxelGridFilter, this);
+  }
+
+  /**
+   * Downsample a PointCloud2 message by applying a voxelgrid filter.
+   *
+   * Given a PointCloud2 message, apply a voxelgrid filter and provide the resulting PointCloud2 message.
+   * More information about pcl filters at: https://pcl.readthedocs.io/projects/tutorials/en/master/#
+   * This filter: https://pcl.readthedocs.io/projects/tutorials/en/latest/voxel_grid.html#voxelgrid
+   *
+   * @param[in] req sensor_msgs/PointCloud2 A PointCloud2 message.
+   * @param[out] res sensor_msgs/PointCloud2 A PointCloud2 message.
+   * @return Bool Service completion result.
+   */
+  bool voxelGridFilter(armada_flexbe_utilities::VoxelGridFilter::Request &req,
+                       armada_flexbe_utilities::VoxelGridFilter::Response &res)
+  {
+    PointCloud<PointXYZRGB>::Ptr input_cloud(new PointCloud<PointXYZRGB>);
+    PointCloud<PointXYZRGB>::Ptr filtered_cloud(new PointCloud<PointXYZRGB>);
+    fromROSMsg(req.cloud_in, *input_cloud);
+
+    nh_.getParam("/filters/voxel_grid/leaf_size_x", leaf_size_x);
+    nh_.getParam("/filters/voxel_grid/leaf_size_y", leaf_size_y);
+    nh_.getParam("/filters/voxel_grid/leaf_size_z", leaf_size_z);
+
+    VoxelGrid<PointXYZRGB> vox;
+    vox.setInputCloud (input_cloud);
+    vox.setLeafSize (leaf_size_x, leaf_size_y, leaf_size_z);
+    vox.filter (*filtered_cloud);
+
+    toROSMsg(*filtered_cloud, res.cloud_out);
+    return true;
+  }
+};
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "voxel_grid_filter_service");
   ros::NodeHandle nh;
 
-  ros::ServiceServer voxelGridFilterService = nh.advertiseService("voxelgrid_filter", voxelGridFilter);
+  VoxelGridFilterService voxelGridFilterService = VoxelGridFilterService(nh);
   ros::spin();
 
   return 0;
