35b7fc647524225db7ccd552e62f5fadb9f46107
Brian Flynn <flynn.m.brian@gmail.com>
2022-12-09 14:59:54 -0500

    dramatically improve speed of processing by adding voxelgrid_filter service on each pointcloud immediately after get_pointcloud service in get_pointcloud state

--- armada_flexbe_states/src/armada_flexbe_states/get_pointcloud_service_state.py
+++ armada_flexbe_states/src/armada_flexbe_states/get_pointcloud_service_state.py
@@ -6,6 +6,7 @@ from flexbe_core.proxy import ProxyServiceCaller
 from sensor_msgs.msg import PointCloud2
 
 from armada_flexbe_utilities.srv import GetPointCloud, GetPointCloudResponse, GetPointCloudRequest
+from armada_flexbe_utilities.srv import PCLVoxelGridFilter, PCLVoxelGridFilterResponse, PCLVoxelGridFilterRequest
 
 
 class GetPointCloudServiceState(EventState):
@@ -33,6 +34,9 @@ class GetPointCloudServiceState(EventState):
                 self._service_topic = '/get_pointcloud'
                 self._service = ProxyServiceCaller({self._service_topic: GetPointCloud})
 
+                self._service_topic_two = '/voxelgrid_filter'
+                self._service_two = ProxyServiceCaller({self._service_topic_two: PCLVoxelGridFilter})
+
         def execute(self, userdata):
                 # This method is called periodically while the state is active.
                 # Main purpose is to check state conditions and trigger a corresponding outcome.
@@ -41,8 +45,9 @@ class GetPointCloudServiceState(EventState):
                 response = GetPointCloudResponse()
 
                 try:
-                    response = self._service.call(self._service_topic, self._camera_topic)
-                    userdata.pointcloud_list.append(response.cloud_out)
+                    get_response = self._service.call(self._service_topic, self._camera_topic)
+                    filter_response = self._service.call(self._service_topic_two, get_response.cloud_out)
+                    userdata.pointcloud_list.append(filter_response.cloud_out)
                     return "continue"
                 except:
                     return "failed"
@@ -65,6 +70,7 @@ class GetPointCloudServiceState(EventState):
                 # because if anything failed, the behavior would not even be started.
 
                 rospy.wait_for_service(self._service_topic)
+                rospy.wait_for_service(self._service_topic_two)
 
         def on_stop(self):
                 # This method is called whenever the behavior stops execution, also if it is cancelled.
