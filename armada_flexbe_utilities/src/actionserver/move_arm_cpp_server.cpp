eca1b6a8175272957e7c6977dcdf143500471d83
test station <test station>
2023-01-10 09:22:39 -0500

    test and validate spawn collision action, should be reworked as service and removed from action once standalone functionality is confirmed

--- armada_flexbe_utilities/src/actionserver/move_arm_cpp_server.cpp
+++ armada_flexbe_utilities/src/actionserver/move_arm_cpp_server.cpp
@@ -19,6 +19,9 @@ protected:
     double x_len;
     double y_len;
     double z_len;
+    double x_pos;
+    double y_pos;
+    double z_pos;
   };
 
   ros::NodeHandle nh_;
@@ -55,6 +58,7 @@ public:
     CartesianMoveServer_.start();
     MoveToNamedPoseServer_.start();
     MoveGroupPtr_ = MoveGroupPtr(new moveit::planning_interface::MoveGroupInterface(planning_group_));
+    PlanningScenePtr_ = PlanningScenePtr(new moveit::planning_interface::PlanningSceneInterface);
     spawnTableCollisionService = nh.advertiseService("spawn_table_collision", &CartesianPlanningCPPAction::spawnTableObstacle, this);
   }
 
@@ -149,38 +153,34 @@ public:
   bool spawnTableObstacle(armada_flexbe_utilities::SpawnTableCollision::Request &req,
                           armada_flexbe_utilities::SpawnTableCollision::Response &res)
   {
-    nh_.getParam("/collision/table/x", table.x_len);
-    nh_.getParam("/collision/table/y", table.y_len);
-    nh_.getParam("/collision/table/z", table.z_len);
-
-    moveit_msgs::CollisionObject collision_surface;
-    collision_surface.header.frame_id = MoveGroupPtr_->getPlanningFrame();
-
-    // The id of the object is used to identify it.
-    collision_surface.id = "box1";
-
-    // Define a box to add to the world.
-    shape_msgs::SolidPrimitive primitive;
-    primitive.type = primitive.BOX;
-    primitive.dimensions.resize(3);
-    primitive.dimensions[primitive.BOX_X] = table.x_len;
-    primitive.dimensions[primitive.BOX_Y] = table.y_len;
-    primitive.dimensions[primitive.BOX_Z] = table.z_len;
-
-    // Define a pose for the box (specified relative to frame_id)
-    geometry_msgs::Pose table_pose;
-    table_pose.orientation.w = 1.0;
-    table_pose.position.x = 0.0;
-    table_pose.position.y = 0.0;
-    double table_offset = table.z_len / 2;
-    table_pose.position.z = 0 - table_offset;
-
-    collision_surface.primitives.push_back(primitive);
-    collision_surface.primitive_poses.push_back(table_pose);
-    collision_surface.operation = collision_surface.ADD;
+    nh_.getParam("/collision/table/dimension/x", table.x_len);
+    nh_.getParam("/collision/table/dimension/y", table.y_len);
+    nh_.getParam("/collision/table/dimension/z", table.z_len);
+    nh_.getParam("/collision/table/pose/x", table.x_pos);
+    nh_.getParam("/collision/table/pose/y", table.y_pos);
+    nh_.getParam("/collision/table/pose/z", table.z_pos);
 
     std::vector<moveit_msgs::CollisionObject> collision_objects;
-    collision_objects.push_back(collision_surface);
+    collision_objects.resize(1);
+
+    collision_objects[0].id = "box1";
+    collision_objects[0].header.frame_id = MoveGroupPtr_->getPlanningFrame();
+
+    collision_objects[0].primitives.resize(1);
+    collision_objects[0].primitives[0].type = collision_objects[0].primitives[0].BOX;
+    collision_objects[0].primitives[0].dimensions.resize(3);
+    collision_objects[0].primitives[0].dimensions[0] = table.x_len;
+    collision_objects[0].primitives[0].dimensions[1] = table.y_len;
+    collision_objects[0].primitives[0].dimensions[2] = table.z_len;
+
+    collision_objects[0].primitive_poses.resize(1);
+    collision_objects[0].primitive_poses[0].position.x = table.x_pos;
+    collision_objects[0].primitive_poses[0].position.y = table.y_pos;
+    collision_objects[0].primitive_poses[0].position.z = table.z_pos;
+    collision_objects[0].primitive_poses[0].orientation.w = 1.0;
+
+    collision_objects[0].operation = collision_objects[0].ADD;
+
     PlanningScenePtr_->addCollisionObjects(collision_objects);
 
     res.success = 1;
