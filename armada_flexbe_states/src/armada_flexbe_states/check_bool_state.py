713673d56452625c9aba67be2e6bc427ba45e8c3
flynnbm <flynn.m.brian@gmail.com>
2023-08-24 01:19:49 -0400

    add new states to handle checking for simulation, making general grasp waypoints from a target pose, and dropping off object slightly higher than picked up for wpi benchmarking tests

---
+++ armada_flexbe_states/src/armada_flexbe_states/check_bool_state.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import rospy
+
+from flexbe_core import EventState, Logger
+from flexbe_core.proxy import ProxyServiceCaller
+
+class CheckBoolState(EventState):
+        '''
+        Example for a state to demonstrate which functionality is available for state implementation.
+        This example lets the behavior wait until the given target_time has passed since the behavior has been started.
+
+        <= true                                        state of bool is true
+        <= false                                       state of bool is false
+
+        '''
+
+        def __init__(self, bool_to_check):
+                # Declare outcomes, input_keys, and output_keys by calling the super constructor with the corresponding arguments.
+                super(CheckBoolState, self).__init__(outcomes = ['true', 'false'])
+
+                self._bool_to_check = bool_to_check
+
+        def execute(self, userdata):
+                # This method is called periodically while the state is active.
+                # Main purpose is to check state conditions and trigger a corresponding outcome.
+                # If no outcome is returned, the state will stay active.
+
+                if self._bool_to_check:
+                    return 'true'
+                else: 
+                    return 'false'
+
+        def on_enter(self, userdata):
+                # This method is called when the state becomes active, i.e. a transition from another state to this one is taken.
+                # It is primarily used to start actions which are associated with this state.
+
+                pass # Nothing to do in this state.
+
+        def on_exit(self, userdata):
+                # This method is called when an outcome is returned and another state gets active.
+                # It can be used to stop possibly running processes started by on_enter.
+
+                pass # Nothing to do in this state.
+
+        def on_start(self):
+                # This method is called when the behavior is started.
+                # If possible, it is generally better to initialize used resources in the constructor
+                # because if anything failed, the behavior would not even be started.
+
+                pass # Nothing to do in this state.
+
+        def on_stop(self):
+                # This method is called whenever the behavior stops execution, also if it is cancelled.
+                # Use this event to clean up things like claimed resources.
+
+                pass # Nothing to do in this state.
---
+++ armada_flexbe_states/src/armada_flexbe_states/grasp_waypoints_service_state.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import rospy
+
+from flexbe_core import EventState, Logger
+from flexbe_core.proxy import ProxyServiceCaller
+
+from armada_flexbe_utilities.srv import GraspWaypoints, GraspWaypointsResponse, GraspWaypointsRequest
+
+
+class GraspWaypointsServiceState(EventState):
+        '''
+        TODO
+
+        ># grasp_candidates                             List of grasp candidates message
+        #> grasp_waypoints_list                         List of sets of grasp waypoints
+
+        <= continue                                     Calculated list of sets of grasp waypoints
+        <= failed                                       Something went wrong
+
+        '''
+
+        def __init__(self):
+                # Declare outcomes, input_keys, and output_keys by calling the super constructor with the corresponding arguments.
+                super(GraspWaypointsServiceState, self).__init__(outcomes = ['continue', 'failed'],
+                                                        input_keys = ['grasp_candidates'],
+                                                        output_keys = ['grasp_waypoints_list'])
+
+                self._service_topic = '/calculate_grasp_waypoints'
+                self._service = ProxyServiceCaller({self._service_topic: GraspWaypoints})
+
+        def execute(self, userdata):
+                # This method is called periodically while the state is active.
+                # Main purpose is to check state conditions and trigger a corresponding outcome.
+                # If no outcome is returned, the state will stay active.
+
+                request = GraspWaypointsRequest()
+                request.pose_list = userdata.grasp_candidates
+
+                try:
+                  service_response = self._service.call(self._service_topic, request)
+                  userdata.grasp_waypoints_list = service_response.grasp_poses_list.poses
+                  return 'continue'
+                except:
+                  return 'failed'
+
+        def on_enter(self, userdata):
+                # This method is called when the state becomes active, i.e. a transition from another state to this one is taken.
+                # It is primarily used to start actions which are associated with this state.
+
+                Logger.loginfo('attempting to generate a list of grasp waypoint sets...' )
+
+        def on_exit(self, userdata):
+                # This method is called when an outcome is returned and another state gets active.
+                # It can be used to stop possibly running processes started by on_enter.
+
+                pass # Nothing to do in this state.
+
+        def on_start(self):
+                # This method is called when the behavior is started.
+                # If possible, it is generally better to initialize used resources in the constructor
+                # because if anything failed, the behavior would not even be started.
+
+                rospy.wait_for_service(self._service_topic)
+
+        def on_stop(self):
+                # This method is called whenever the behavior stops execution, also if it is cancelled.
+                # Use this event to clean up things like claimed resources.
+
+                pass # Nothing to do in this state.
